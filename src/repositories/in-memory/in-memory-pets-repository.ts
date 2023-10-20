8dac79f9cd9c29b4bd9c93201ac3d6cb9d9ceb80
felipesanderp <felipe.panisson@hotmail.com>
2023-10-20 11:16:57 -0300

    get pests by id and by query

--- src/repositories/in-memory/in-memory-pets-repository.ts
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -1,9 +1,55 @@
 import { Pet, Prisma } from '@prisma/client'
-import { PetsRepository } from '../pets-repository'
+import { GetPetsQuery, PetsRepository } from '../pets-repository'
 import { randomUUID } from 'node:crypto'
+import paginate from '@/utils/paginate'
 
 export class InMemoryPetsRepository implements PetsRepository {
   public items: Pet[] = []
+
+  async findById(id: string) {
+    const pet = this.items.find((item) => item.id === id)
+
+    if (!pet) {
+      return null
+    }
+
+    return pet
+  }
+
+  async getPets(query: GetPetsQuery, page = 1) {
+    const petsFiltered = this.items.filter((item) => {
+      if (query.age && query.age !== item.age) {
+        return false
+      }
+
+      if (query.size && query.size !== item.size) {
+        return false
+      }
+
+      if (query.levelOfEnergy && query.levelOfEnergy !== item.levelOfEnergy) {
+        return false
+      }
+
+      if (
+        query.levelOfIndependence &&
+        query.levelOfIndependence !== item.levelOfIndependence
+      ) {
+        return false
+      }
+
+      if (query.environment && query.environment !== item.environment) {
+        return false
+      }
+
+      if (query.org_id && query.org_id !== item.org_id) {
+        return false
+      }
+
+      return true
+    })
+
+    return paginate<Pet>(petsFiltered, page)
+  }
 
   async create(data: Prisma.PetUncheckedCreateInput) {
     const pet = {
--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,5 +1,16 @@
 import { Prisma, Pet } from '@prisma/client'
 
+export interface GetPetsQuery {
+  age?: Pet['age']
+  size?: Pet['size']
+  levelOfEnergy?: Pet['levelOfEnergy']
+  levelOfIndependence?: Pet['levelOfIndependence']
+  environment?: Pet['environment']
+  org_id?: Pet['org_id']
+}
+
 export interface PetsRepository {
+  findById(id: string): Promise<Pet | null>
+  getPets(query: GetPetsQuery, page?: number): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
 }
