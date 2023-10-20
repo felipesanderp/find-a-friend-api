471e2043e89d5e294328ba33aa49b6bccf8df855
felipesanderp <felipe.panisson@hotmail.com>
2023-10-20 14:24:34 -0300

    prisma pets repo

---
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -0,0 +1,40 @@
+import { Prisma } from '@prisma/client'
+import { GetPetsQuery, PetsRepository } from '../pets-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaPetsRepository implements PetsRepository {
+  async findById(id: string) {
+    const pet = await prisma.pet.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return pet
+  }
+
+  async getPets(query: GetPetsQuery, page?: number | undefined) {
+    const pets = await prisma.pet.findMany({
+      where: {
+        age: query.age,
+        size: query.size,
+        levelOfEnergy: query.levelOfEnergy,
+        levelOfIndependence: query.levelOfIndependence,
+        environment: query.environment,
+        org_id: query.org_id,
+      },
+      skip: page ? (page - 1) * 10 : undefined,
+      take: 10,
+    })
+
+    return pets
+  }
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({
+      data,
+    })
+
+    return pet
+  }
+}
