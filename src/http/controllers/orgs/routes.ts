5f4083e8c03b46f7255337bd1b87f5409e9a1d55
felipesanderp <felipe.panisson@hotmail.com>
2024-06-06 12:12:56 -0300

    docs: Org Auth docs

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,12 +1,67 @@
 import { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 import { authenticate } from './authenticate.controller'
 import { register } from './register.controller'
 import { refresh } from './refresh.controller'
 
 export async function orgRoutes(app: FastifyInstance) {
-  app.post('/orgs/register', register)
-  app.post('/orgs/sessions', authenticate)
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/orgs/register',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Register as a Org',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string().min(6),
+          address: z.string(),
+          name: z.string(),
+          phone: z.string(),
+          zipcode: z.string(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    register,
+  )
 
-  app.patch('/token/refresh', refresh)
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/orgs/sessions',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Authenticate with e-mail & password',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string(),
+        }),
+        response: {
+          200: z.object({
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    authenticate,
+  )
+
+  app.withTypeProvider<ZodTypeProvider>().patch(
+    '/token/refresh',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Refresh token',
+        response: {
+          200: z.object({
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    refresh,
+  )
 }
