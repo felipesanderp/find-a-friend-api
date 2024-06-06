2bc5e8007bdc975b7d63a302ab6c11e8365ae8fc
felipesanderp <felipe.panisson@hotmail.com>
2024-06-06 12:02:37 -0300

    docs: Swagger config with zod type validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import fastifyJwt from '@fastify/jwt'
 import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
 import { env } from './env'
 import { orgRoutes } from './http/controllers/orgs/routes'
 import { petsRoutes } from './http/controllers/pets/routes'
 
-export const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -20,6 +32,30 @@ app.register(fastifyJwt, {
 })
 
 app.register(fastifyCookie)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'FindAFriend API',
+      description: 'API built in with Fastify',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(orgRoutes)
 app.register(petsRoutes)
